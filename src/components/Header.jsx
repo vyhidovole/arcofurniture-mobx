f98674f079a8d5475a4d9e4d27f11478c853e167
oleg <vyhidovoleg@gmail.com>
2025-01-25 16:11:54 +0300

    header
    header's correction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 const Header = () => {
-    return ( <div className="container flex justify-between">
+    return ( <div className="container flex justify-between pt-4">
         <Image
           src={"/images/logo.jpg"}
           alt="арко"
