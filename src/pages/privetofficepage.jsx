467b14bf375b7b89f0acd349416eef42e19e84ba
oleg <vyhidovoleg@gmail.com>
2025-02-05 15:29:11 +0300

    Create privetofficepage

---
+++ src/pages/privetofficepage.jsx
@@ -0,0 +1,30 @@
+import React, { useState, } from "react"
+import Link from "next/link";
+import ModalPrivetOffice from "@/components/Modal/ModalPrivetOffice";
+
+const PrivetOffice = () => {
+    const [isEntryModalOpen, setEntryModalOpen] = useState(true); // Состояние для ModalPrivetOffice
+
+
+    const closeEntryDialog = () => {
+        setEntryModalOpen(false); // Закрываем модальное окно для входа
+    };
+    return (
+        <>
+        <div className="flex mt-6 ">
+                <Link href="/" className="text-base hover:underline mr-1">Главная</Link>
+                -
+                <Link href="/privetofficepage" className="text-base hover:underline ml-1 mr-1">Личный кабинет</Link>
+                -
+                <p className="text-base ml-1">Авторизация</p>
+
+            </div>
+            
+            <ModalPrivetOffice show={isEntryModalOpen} onClose={closeEntryDialog} />{/* Добавляем модальное окно */}
+
+        </>
+
+    );
+}
+
+export default PrivetOffice
