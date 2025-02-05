6382f90d81c0b46a29c5f24aaf48b391308baf69
oleg <vyhidovoleg@gmail.com>
2025-02-05 15:28:31 +0300

    Create forgot-password page

---
+++ src/pages/forgot-password.jsx
@@ -0,0 +1,73 @@
+import React, { useState, } from "react"
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+
+const Password = () => {
+     const router = useRouter();
+    const [email, setEmail] = useState('')
+    const handleChange=(e)=>setEmail(e.target.value)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        //создаем объект с данными для отправки
+        const formDta = {
+            email: email,
+        }
+
+        //записывает данные в localStorage
+        localStorage.setItem("formData", JSON.stringify(formDta))
+        console.log("Данные сохранены в localStorage:", formDta)
+       
+       //очищаем поле
+       setEmail('')
+       setTimeout(()=>{
+         alert("Новый пароль был выслан на ваш адрес электронной почты.")
+       },0)
+       
+         
+
+    }
+
+
+    return (
+        <>
+            <h2 className="text-3xl font-semibold ">Забыли пароль?</h2>
+            <div className="flex">
+                <Link href="/" className="text-base hover:underline mr-1">Главная</Link>-
+                <Link href="/privetofficepage" className="text-base hover:underline ml-1 mr-1">Личный кабинет</Link>
+                -
+                <p className="text-base ml-1">Забыли пароль?</p>
+            </div>
+
+
+
+            <form
+                onSubmit={handleSubmit}
+                method="dialog"
+            >
+                <div className="inline-flex flex-col mt-6">
+                    <label htmlFor="">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={email} 
+                        onChange={handleChange}
+                        className="text-center outline-none bg-zinc-300  rounded-lg "
+                    />
+                </div>
+                <div className="mt-6">
+                    <Link href="/privetofficepage"  className="bg-zinc-300 px-4 py-2 rounded-lg mr-3">назад</Link>
+                    
+                    <button type="submit" className="bg-zinc-300 px-4 py-2 rounded-lg m-3">продолжить</button>
+                </div>
+
+            </form>
+
+        </>
+
+    );
+}
+
+export default Password;
+
