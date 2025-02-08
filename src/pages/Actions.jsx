9bc2689b9921092b8db7b9337633351bfdaa55d5
oleg <vyhidovoleg@gmail.com>
2025-02-08 12:52:19 +0300

    removed title of the pge

--- src/pages/Actions.jsx
+++ src/pages/Actions.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { CiGrid41 } from "react-icons/ci";
+import CustomSlider2 from '@/components/Slider/CustomSlider2';
+
+
 
 const Actions = () => {
-  return <h3>This is Actions page</h3>;
+  return (
+    <>
+    <div className="bg-gray-400 w-40 p-4 font-medium text-lg text-white rounded-md flex justify-around mb-6">
+      <CiGrid41 />
+      <h2 className="text-base">Акции</h2>
+      </div>
+      {/* Slider Component */}
+      
+      <CustomSlider2 />
+    </>
+  
+)
 };
 
 export default Actions;
