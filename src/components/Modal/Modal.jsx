2ef7cbcceecb3cfab2f67ba76436729baf0d6b68
oleg <vyhidovoleg@gmail.com>
2025-02-08 12:42:03 +0300

    z index set

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -23,7 +23,7 @@ const Modal = ({ isOpen, onClose }) => {
   }, []); // Пустой массив зависимостей, чтобы эффект выполнялся только при монтировании и размонтировании
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
       <div className="bg-white p-4 rounded shadow-lg w-3/4 h-5/6 flex flex-col">
         
         {/* Заголовок модального окна */}
