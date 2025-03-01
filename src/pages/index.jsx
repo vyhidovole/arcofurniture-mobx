6c509fc552383deb21de97d710ef03f6be91e8c9
oleg <vyhidovoleg@gmail.com>
2025-03-01 08:35:56 +0300

    replace Head

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,22 @@
-import Head from "next/head";
 import React from "react";
+import Head from "next/head";
+import Catalogue from "@/components/Catalogue";
+import MenuBar from "@/components/MenuBar";
 
 /* Домашнаяя страница */
 const Home = () => (
+  <>
   <Head>
     <title>Arcofurniture</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
+  <MenuBar/>
+  <Catalogue/>
+  </>
+  
+  
 );
 
 export default Home;
