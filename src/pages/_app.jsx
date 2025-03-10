eda8bfa515ffffc8f7e5a74cd1a3f03b7761238e
oleg <vyhidovoleg@gmail.com>
2025-03-10 08:43:48 +0300

    mport MenuBar from "@/components/MenuBar";

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import React from "react";
 import MainLayout from "../Layouts/MainLayout";
 import { Provider } from "@/components/ui/provider";
+import MenuBar from "@/components/MenuBar";
 
 /* Корневой элемент страницы */
 const App = ({ Component, pageProps }) => {
   return (
     <Provider>
 <MainLayout>
+<MenuBar />
       <Component {...pageProps} />
     </MainLayout>
    
