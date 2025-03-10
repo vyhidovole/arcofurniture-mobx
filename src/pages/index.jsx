bd56b2ca36e787e943dc2986deb4838ca1979687
oleg <vyhidovoleg@gmail.com>
2025-03-10 08:44:29 +0300

    index fixeed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Catalogue from "@/components/Catalogue";
-import MenuBar from "@/components/MenuBar";
+
 
 /* Домашнаяя страница */
 const Home = () => (
@@ -12,8 +12,7 @@ const Home = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
-  <MenuBar/>
-  <Catalogue/>
+ <Catalogue/>
   </>
   
   
