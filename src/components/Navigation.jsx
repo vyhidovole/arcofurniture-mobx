6bc6477b7fe26f9880959658a63979013fc932b4
oleg <vyhidovoleg@gmail.com>
2025-03-25 09:24:39 +0300

    set Them and switch to toggle Them in the  project

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { useTheme } from '@/context/ThemeContext';
 import '@/components/Header.css';
 
 /* пункты меню в шапке */
@@ -16,10 +17,19 @@ const navItems = [
 
 
 const Navigation = () => {
+
+  const { isDarkMode, toggleTheme } = useTheme(); // Получаем доступ к теме
   // состояние (стейт) для активного пункта меню
-  const [activeLink, setActiveLink] = useState("Home");
+  const [activeLink, setActiveLink] = useState("Главная");
 
   const router = useRouter();
+  useEffect(() => {
+    const currentPath = router.pathname;
+    const activeItem = navItems.find(item => item.path === currentPath);
+    if (activeItem) {
+      setActiveLink(activeItem.name);
+    }
+  }, [router.pathname]);
 
   // клик по активному пункту меню
   const onClickHandler = (link, path) => {
@@ -27,18 +37,21 @@ const Navigation = () => {
       router.push(path);
       setActiveLink(link);
     }
+    console.log(activeLink)
   };
 
   return (
-    <header className="shadow bg-white h-16 flex items-center">
+    <header className={`shadow ${isDarkMode ? 'bg-gray-800' : 'bg-white'} h-16 flex items-center`}>
       <div className="container flex justify-between">
         {/* Левая часть с первыми пятью пунктами меню */}
         <nav className="flex items-center gap-5">
           {navItems.slice(0, 5).map((item) => (
             <a
               onClick={() => onClickHandler(item.name, item.path)}//передаем путь
-              className={`nav-link cursor-pointer ${item.name === activeLink ? "text-sky-500" : ""
-                }`}
+              className={`nav-link cursor-pointer
+                 ${item.name === activeLink ?
+                  "text-sky-500  underline-animation " : "text-gray-800"
+                }${isDarkMode ? 'dark:text-sky-500' : 'text-gray-800'} `}
               key={item.path}
             >
               {item.name}
@@ -51,13 +64,29 @@ const Navigation = () => {
           {navItems.slice(5).map((item) => (
             <a
               onClick={() => onClickHandler(item.name, item.path)}
-              className={`nav-link cursor-pointer ${item.name === activeLink ? "text-sky-500" : ""
+              className={`nav-link cursor-pointer 
+                ${item.name === activeLink ? "text-sky-500 underline-animation " : "text-gray-800"
                 }`}
               key={item.path}
             >
               {item.name}
             </a>
           ))}
+          {/* Переключатель темы */}
+          <fieldset className="relative inline-block w-16 h-8">
+            <input
+              type="checkbox"
+              className="switch opacity-0 w-0 h-0"
+              id="theme-toggle"
+              checked={isDarkMode}
+              onChange={toggleTheme}
+            />
+            <label
+              htmlFor="theme-toggle"
+              className={`absolute cursor-pointer top-0 left-0 right-0 bottom-0 rounded-full transition duration-300 ${isDarkMode ? 'bg-blue-600' : 'bg-gray-300'}`}
+            ></label>
+            <span onClick={toggleTheme} className={`absolute left-1 top-1 block w-6 h-6 bg-white rounded-full transition-transform duration-300 ${isDarkMode ? 'translate-x-8' : ''}`}></span>
+          </fieldset>
         </nav>
       </div>
     </header>
