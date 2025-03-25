eba4ef4f1a1bc969f7c5af7d118d6fb4a2e6044c
oleg <vyhidovoleg@gmail.com>
2025-03-25 09:19:26 +0300

    fixed Modals to validate  and submit properly

--- src/components/Modal/ModalEntry.jsx
+++ src/components/Modal/ModalEntry.jsx
@@ -1,111 +1,143 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Alert from "../Alert/Alert";
+import useForm from "@/hooks/useForm";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
 
-const ModalEntry = ({ show, onClose}) => {
-   
+const ModalEntry = ({ show, onClose, setNewForm }) => {
+    const { formData, errors, handleChange, handleSubmit, resetForm } = useForm(
+        {
+            name: "",
+            email: "",
+            password: "",
+        },
+        setNewForm
+    );
+
     const router = useRouter();
-    const dialogRef = useRef(null)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-   
+    const dialogRef = useRef(null);
+    const [isShowAlert, setShowAlert] = useState(false);
 
     useEffect(() => {
         if (show) {
             dialogRef.current.show();
-            document.addEventListener('mousedown', handleBackgroundClick);
+            document.addEventListener("mousedown", handleBackgroundClick);
         } else {
             dialogRef.current.close();
-            document.removeEventListener('mousedown', handleBackgroundClick);
+            document.removeEventListener("mousedown", handleBackgroundClick);
         }
 
         return () => {
-            document.removeEventListener('mousedown', handleBackgroundClick);
+            document.removeEventListener("mousedown", handleBackgroundClick);
         };
     }, [show]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault() //предотвращаем перезагрузку страницы
-
-        //создаём объект с данными для отправки
-        const formData = {
-            email: email,
-            password: password,
-        }
-        //Сохраняем данные в localStorage
-        localStorage.setItem('formData', JSON.stringify(formData))
-        console.log('Данные сохранены в localStorage:', formData)
-
-        //очищаем поля
-        setEmail('')
-        setPassword('')
-
-        //закрываем диалоговое окно
-        onClose()
-    }
-    //обработчик события для закрытия диалоговго окна
+    // Обработчик события для закрытия диалогового окна
     const handleBackgroundClick = (e) => {
         // Check if the click target is not the dialog or its children
         if (dialogRef.current && !dialogRef.current.contains(e.target)) {
+            // Очищаем поля
+            resetForm();
             onClose(); // Close the modal
         }
+    };
 
-    }
+    const handleFormSubmit = async (e) => {
+        e.preventDefault(); // Предотвращаем перезагрузку страницы
+        // Проверяем наличие ошибок
+        if (Object.keys(errors).length === 0) {
+            // Если нет ошибок, отправляем данные
+            const isSuccess = await handleSubmit(e); // Предполагается, что handleSubmit возвращает true/false
 
+            // Если форма успешно отправлена и нет ошибок
+            if (isSuccess) {
+                localStorage.setItem("userData", JSON.stringify(formData));
 
+                setShowAlert(true);
+                resetForm(); // Сбрасываем форму
+
+                setTimeout(() => {
+                    setShowAlert(false);
+                    onClose();
+                }, 3000); // Закрываем алерт через 3 секунды
+            }
+        } else {
+            // Если есть ошибки, вы можете обработать это здесь
+            console.error("Форма содержит ошибки:", errors);
+        }
+    };
 
     return (
-        <div className={`fixed inset-0  bg-black bg-opacity-50  flex justify-center z-10 items-center ${show ? 'block' : 'hidden'}`} onClick={handleBackgroundClick} // Добавляем обработчик клика
+        <div
+            className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center z-10 items-center ${show ? "block" : "hidden"}`}
+            onClick={handleBackgroundClick} // Добавляем обработчик клика
         >
             <dialog ref={dialogRef} className="rounded-xl">
-
-                <form onSubmit={handleSubmit} method="dialog" >
-                    <div className="bg-white p-4  shadow-lg w-11/12 md:w-3/4 lg:w-72 h-96 flex flex-col " onClick={(e) => e.stopPropagation()}>{/* Останавливаем всплытие клика на модалке */}
+                <form onSubmit={handleFormSubmit} method="dialog">
+                    <div
+                        className={` bg-white p-4 shadow-lg w-11/12 md:w-3/4 lg:w-72 h-96 flex flex-col`}
+                        onClick={(e) => e.stopPropagation()} // Останавливаем всплытие клика на модалке
+                    >
                         {/* Заголовок Модального окна */}
-                        <div className=" bottom-4 text-4xl ml-16">
-                            <h1 className="font-bold ">Войти</h1>
+                        <div className="bottom-4 text-4xl ml-16">
+                            <h1 className="font-bold">Войти</h1>
                         </div>
 
                         {/* Содержание Модального окна */}
+                        <Input
+                            className={errors.name ? "border-red-500" : ""}
+                            label="Name"
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            error={errors.name}
+                        />
 
-                        <input
+                        <Input
+                            className={errors.email ? "border-red-500" : ""}
+                            label="Email"
                             type="email"
                             name="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)} // Обновление состояния для имени 
-                            placeholder="E-Mail"
-                            required
-                            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-gray-700 mt-5"
+                            value={formData.email}
+                            onChange={handleChange}
+                            error={errors.email}
                         />
-                        <input
+
+                        <Input
+                            className={errors.password ? "border-red-500" : ""}
+                            label="Password"
                             type="password"
                             name="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)} // Обновление состояния для телефона
-                            placeholder="Ваш телефон"
-                            required
-                            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-gray-700 mt-5"
+                            value={formData.password}
+                            onChange={handleChange}
+                            error={errors.password}
                         />
-                        <Link href="/forgot-password" className="mt-6 cursor-pointer" onClick={onClose}><p>Забыли пароль?</p></Link>
-                        <button
-                            type="submit"
-                            className="with-full p-4 bg-slate-600 text-white rounded-xl mt-6 cursor-pointer">Войти
-                        </button>
-                        <div className=" flex justify-between  items-center">
-                            <p className="text-base text-center mt-6 ">Нет аккаунта?
-                            </p>
-                            <Link href="/account-page" className="mt-6 cursor-pointer" onClick={onClose}>
-                                <p >Создать</p>
-                            </Link>
-                        </div>
+                        <Link href="/forgot-password" className="mt-6 cursor-pointer" onClick={onClose}>
+                            <p>Забыли пароль?</p>
+                        </Link>
 
+                        <Button type="submit" variant="secondary">
+                            Отправить
+                        </Button>
+
+                        {isShowAlert && (
+                            <Alert
+                                variant="positive"
+                                isOpen={isShowAlert}
+                                onClose={() => setTimeout(() => setShowAlert(false), 3000)} // Передаем функцию
+                            >
+                                <h2>Авторизация прошла успешно!</h2>
+                                <p>Вы авторизированны!</p>
+                            </Alert>
+                        )}
                     </div>
                 </form>
-
             </dialog>
         </div>
     );
-}
+};
 
 export default ModalEntry;
--- src/components/Modal/ModalPrivetOffice.jsx
+++ src/components/Modal/ModalPrivetOffice.jsx
@@ -1,64 +1,56 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Alert from "../Alert/Alert";
 import useForm from "@/hooks/useForm";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 
-const ModalPrivetOffice = ({ onClose }) => {
-    const { formData, errors, handleChange, handleSubmit } = useForm({
+const ModalPrivetOffice = ({ setNewState }) => {
+    const { formData, errors, handleChange, handleSubmit,resetForm } = useForm({
         name: "",
         email: "",
         password: "",
-    });
-    // const dialogRef = useRef(null)
-    //  const [email, setEmail] = useState('')
-    // const [password, setPassword] = useState('')
-    // Стейт для закрытия компонента уведомления
+    }, setNewState);
+
     const [isShowAlert, setShowAlert] = useState(false);
+    
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault() //предотвращаем перезагрузку страницы
+    const handleFormSubmit = async (e) => {
+        e.preventDefault(); // Предотвращаем перезагрузку страницы
 
-    //     //создаём объект с данными для отправки
-    //     const formData = {
-    //         email: email,
-    //         password: password,
-    //     }
-    //     //Сохраняем данные в localStorage
-    //     localStorage.setItem('formData', JSON.stringify(formData))
-    //     console.log('Данные сохранены в localStorage:', formData)
+        // Вызываем handleSubmit из useForm для отправки данных
+        const isSuccess = await handleSubmit(e); // Предполагается, что handleSubmit возвращает true/false
 
-    //     //очищаем поля
-    //     setEmail('')
-    //     setPassword('')
-
-    //     //закрываем диалоговое окно
-    //     onClose()
-    // }
-
-
-
+        // Если форма успешно отправлена и нет ошибок
+        if (isSuccess && Object.keys(errors).length === 0) {
+            localStorage.setItem('userData', JSON.stringify(formData));
+           
+            setShowAlert(true);
+            resetForm(); // Сбрасываем форму
+            setTimeout(() => {
+                setShowAlert(false);
+            }, 3000); // Закрываем алерт через 3 секунды
+        }
+    };
 
     return (
-        <form onSubmit={handleSubmit} method="dialog" >
-            <div className="bg-white p-4  shadow-lg w-72 h-96 flex flex-col mt-6 " >
+        <form onSubmit={handleFormSubmit} method="dialog" >
+            <div className="${isDarkMode ? 'bg-gray-800' : 'bg-white'}p-4  shadow-lg w-72 h-96 flex flex-col mt-6 " >
                 {/* Заголовок Модального окна */}
                 <div className=" bottom-4 text-4xl ml-16">
                     <h1 className="font-bold ">Войти</h1>
                 </div>
+                <Input
+                    className={errors.name ? "border-red-500" : ""}
+                    label="Name"
+                    type="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    error={errors.name}
+                />
 
-                {/* Содержание Модального окна */}
 
-                {/* <input
-                    type="email"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} // Обновление состояния для имени 
-                    placeholder="E-Mail"
-                    required
-                    className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-gray-700 mt-5"
-                /> */}
                 <Input
                     className={errors.email ? "border-red-500" : ""}
                     label="Email"
@@ -81,13 +73,13 @@ const ModalPrivetOffice = ({ onClose }) => {
                 <Link href="/forgot-password" className="mt-6 cursor-pointer"><p>Забыли пароль?</p>
                 </Link>
                 <Button
-                    onClick={() => setShowAlert(true)}
+                    type="submit"
                     variant="secondary">Отправить
                 </Button>
 
                 <div className=" flex justify-between  items-center">
-                    <p className="text-base text-center mt-6 ">Нет аккаунта?</p>
-                    <Link href="/account-page" className="mt-6 cursor-pointer">
+                    <p className="text-base text-center mt-10 ">Нет аккаунта?</p>
+                    <Link href="/account-page" className="mt-10 cursor-pointer">
                         <p >Создать</p>
                     </Link>
 
