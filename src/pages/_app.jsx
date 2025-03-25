06935b8a2c3c137d15d3fe05321d91bb30a45b3d
oleg <vyhidovoleg@gmail.com>
2025-03-25 09:32:15 +0300

    ThemeProvider added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,18 +4,26 @@ import MainLayout from "../Layouts/MainLayout";
 import { Provider } from "@/components/ui/provider";
 import MenuBar from "@/components/MenuBar";
 import { CartProvider } from '@/context/CartContext';
+import { ThemeProvider } from '@/context/ThemeContext'; 
+
+
+
 
 /* Корневой элемент страницы */
 const App = ({ Component, pageProps }) => {
+ 
   return (
-    <Provider>
-      <CartProvider>
-        <MainLayout>
-          <MenuBar />
-          <Component {...pageProps} />
-        </MainLayout>
-      </CartProvider>
-    </Provider>
+    <ThemeProvider>
+      <Provider>
+        <CartProvider>
+          <MainLayout>
+            <MenuBar />
+            <Component {...pageProps} />
+          </MainLayout>
+        </CartProvider>
+      </Provider>
+    </ThemeProvider>
+
 
   );
 };
