bb058625c595165cb0387df2da1c87a94bc11ddc
oleg <vyhidovoleg@gmail.com>
2025-03-25 09:37:57 +0300

    import added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import createRoot instead of ReactDOM
 import App from './App';
+import './Header.css'; 
+import './globals.css';
 
 const container = document.getElementById('app'); // Get the root container
 const root = ReactDOM.createRoot(container); // Create a root
