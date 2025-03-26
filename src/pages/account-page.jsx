6374b59c8b1fe38e22aac0202744f0a7faa919e8
oleg <vyhidovoleg@gmail.com>
2025-03-26 17:47:07 +0300

    set validated Input

--- src/pages/account-page.jsx
+++ src/pages/account-page.jsx
@@ -1,38 +1,60 @@
 import React, { useState, } from "react"
-import { useRouter } from "next/router";
+import useForm from "@/hooks/useForm";
 import Link from "next/link";
 import Alert from "@/components/Alert/Alert";
+import Input from "@/components/Input/Input";
+import Button from "@/components/Button/Button";
 
 
 
-const Account = () => {
-    const router = useRouter();
-    //создаем один объект состоянияformData
-    const [formData, setFormdata] = useState({ name: '', tel: '', email: '', password: '', confirmation: '' })
-     const [alertVisible, setAlertVisible] = useState(false);
-        const [alertMessage, setAlertMessage] = useState('');
-        const [alertVariant, setAlertVariant] = useState('info');
-    const handleChange = (e) => {
-        const { name, value } = e.target//получаем имя и значение поля
-        setFormdata({ ...formData, [name]: value })// обновляем только нужное поле
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        //Сохраняем данные в localStorage
-        localStorage.setItem('formData', JSON.stringify(formData))
-        console.log('Данные сохранены в localStorage:', formData)
-        //очищаем поля формы, устанавливаем начальное состояние
-        setFormdata({ name: '', tel: '', email: '', password: '', confirmation: '' })
-
-         // Устанавливаем сообщение и показываем Alert
-       setAlertMessage("Вы успешно зарегистрировались");
-       setAlertVariant('positive'); // Установите нужный вариант
-       setAlertVisible(true); 
-       setTimeout(()=>{
-        setAlertVisible(false)
-       },3000) 
-    }
+const Account = (setNewState) => {
     
+    const { formData, errors, handleChange, handleSubmit, resetForm } = useForm(
+        { name: '', tel: '', email: "", password: '', confirmation: '' }, setNewState);
+   
+    const [isShowAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertVariant, setAlertVariant] = useState('info');
+    
+    const handleFormSubmit = async (e) => {
+        e.preventDefault(); // Предотвращаем перезагрузку страницы
+
+        // Вызываем handleSubmit из useForm для отправки данных
+        const isSuccess = await handleSubmit(e); // Предполагается, что handleSubmit возвращает true/false
+
+        // Если форма успешно отправлена и нет ошибок
+        if (isSuccess && Object.keys(errors).length === 0) {
+            localStorage.setItem('userData', JSON.stringify(formData));
+
+            setShowAlert(true);
+            resetForm(); // Сбрасываем форму
+            setTimeout(() => {
+                setShowAlert(false);
+            }, 3000); // Закрываем алерт через 3 секунды
+        } else {
+            // Устанавливаем сообщение и показываем Alert
+        setAlertMessage("Данные введены не корректно.");
+        setAlertVariant('negative'); // Установите нужный вариант
+        setShowAlert(true);
+        setTimeout(() => {
+            setShowAlert(false)
+        }, 3000)
+        return
+        }
+        // Устанавливаем сообщение и показываем Alert
+        setAlertMessage("Регисрация прошла успешно.");
+        setAlertVariant('positive'); // Установите нужный вариант
+        setShowAlert(true);
+        setTimeout(() => {
+            setShowAlert(false)
+        }, 3000)
+
+    }
+
+    const handleCloseAlert = () => {
+        setShowAlert(false);
+    };
+
 
     return (
         <>
@@ -42,69 +64,82 @@ const Account = () => {
                 -
                 <p className="text-base ml-1">Быстрая регистрация</p>
             </div>
-            <Alert 
-                isOpen={alertVisible} 
-                
+            <Alert
+                isOpen={isShowAlert}
+                onClose={handleCloseAlert}
                 variant={alertVariant}
             >
                 {alertMessage}
             </Alert>
             <form
-                onSubmit={handleSubmit}
+                onSubmit={handleFormSubmit}
                 method="dialog"
                 className="sm:inline-flex flex-col lg:flex-wrap">
                 <div className="inline-flex flex-col m-6">
-                    <label htmlFor="">ФИО</label>
-                    <input
+                    
+                    <Input
+                        className={errors.name ? "border-red-500" : ""}
+                        label="Name"
                         type="text"
                         name="name"
                         value={formData.name}
                         onChange={handleChange}
-                        style={{ textAlign: 'center' }} // Центрируем текст
-                        className="bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-violet-300 " />
+                        error={errors.name}
+                    />
                 </div>
                 <div className="inline-flex flex-col m-6">
-                    <label htmlFor="">Телефон</label>
-                    <input
+                   
+                    <Input
+                        className={errors.email ? "border-red-500" : ""}
+                        label="Телефон"
                         type="tel"
                         name="tel"
                         value={formData.tel}
                         onChange={handleChange}
-                        style={{ textAlign: 'center' }} // Центрируем текст
-                        className="bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-violet-300 " />
+                        error={errors.tel}
+                    />
                 </div>
                 <div className="inline-flex flex-col m-6">
-                    <label htmlFor="">Email</label>
-                    <input
+                    
+                    <Input
+                        className={errors.email ? "border-red-500" : ""}
+                        label="Email"
                         type="email"
                         name="email"
                         value={formData.email}
                         onChange={handleChange}
-                        style={{ textAlign: 'center' }} // Центрируем текст
-                        className="bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-violet-300 " />
+                        error={errors.email}
+                    />
                 </div>
                 <div className="inline-flex flex-col m-6">
-                    <label htmlFor="">Пароль</label>
-                    <input
+                   
+                    <Input
+                        className={errors.password ? "border-red-500" : ""}
+                        label="Password"
                         type="password"
                         name="password"
                         value={formData.password}
                         onChange={handleChange}
-                        style={{ textAlign: 'center' }} // Центрируем текст
-                        className="bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-violet-300 " />
+                        error={errors.password}
+                    />
                 </div>
                 <div className="inline-flex flex-col m-6">
-                    <label htmlFor="">Подтвердите пароль</label>
-                    <input
-                        type="password"
-                        name="confirmation"
-                        value={formData.confirmation}
-                        onChange={handleChange}
-                        style={{ textAlign: 'center' }} // Центрируем текст
-                        className="bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-violet-300 " />
+                    
+                        <Input
+                    className={errors.password ? "border-red-500" : ""}
+                    label="Confirmation"
+                    type="password"
+                    name="confirmation"
+                    value={formData.confirmation}
+                    onChange={handleChange}
+                    error={errors.password}
+                />
                 </div>
 
-                <button type="submit" className="bg-blue-400 text-white p-3 rounded-md">Продолжить</button>
+                {/* <button type="submit" className="bg-blue-400 text-white p-3 rounded-md">Продолжить</button> */}
+                <Button type="submit" variant="secondary">
+                        Отправить
+                    </Button>
             </form>
 
         </>
--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -1,45 +1,61 @@
 import React, { useState, } from "react"
-import { useRouter } from "next/router";
 import Link from "next/link";
+import useForm from "@/hooks/useForm";
 import Alert from "@/components/Alert/Alert";
+import Input from "@/components/Input/Input";
+import Button from "@/components/Button/Button";
 
 
-const Password = () => {
-    //  const router = useRouter();
-    const [email, setEmail] = useState('')
-    const [alertVisible, setAlertVisible] = useState(false);
+const Password = (setNewState) => {
+
+    const { formData, errors, handleChange, handleSubmit, resetForm } = useForm(
+        { email: "", }, setNewState);
+
+
+
+
+    const [isShowAlert, setShowAlert] = useState(false);
     const [alertMessage, setAlertMessage] = useState('');
     const [alertVariant, setAlertVariant] = useState('info');
-    
-    const handleChange=(e)=>setEmail(e.target.value)
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        //создаем объект с данными для отправки
-        const formDta = {
-            email: email,
+    // const handleChange = (e) => setEmail(e.target.value)
+    const handleFormSubmit = async (e) => {
+        e.preventDefault(); // Предотвращаем перезагрузку страницы
+
+        // Вызываем handleSubmit из useForm для отправки данных
+        const isSuccess = await handleSubmit(e); // Предполагается, что handleSubmit возвращает true/false
+
+        // Если форма успешно отправлена и нет ошибок
+        if (isSuccess && Object.keys(errors).length === 0) {
+            localStorage.setItem('userData', JSON.stringify(formData));
+
+            setShowAlert(true);
+            resetForm(); // Сбрасываем форму
+            setTimeout(() => {
+                setShowAlert(false);
+            }, 3000); // Закрываем алерт через 3 секунды
+        } else {
+            // Устанавливаем сообщение и показываем Alert
+            setAlertMessage("Данные введены не корректно.");
+            setAlertVariant('negative'); // Установите нужный вариант
+            setShowAlert(true);
+            setTimeout(() => {
+                setShowAlert(false)
+            }, 3000)
+            return
         }
-
-        //записывает данные в localStorage
-        localStorage.setItem("formData", JSON.stringify(formDta))
-        console.log("Данные сохранены в localStorage:", formDta)
-       
-       //очищаем поле
-       setEmail('')
-    
-       
-       // Устанавливаем сообщение и показываем Alert
-       setAlertMessage("Новый пароль был выслан на ваш адрес электронной почты.");
-       setAlertVariant('positive'); // Установите нужный вариант
-       setAlertVisible(true); 
-       setTimeout(()=>{
-        setAlertVisible(false)
-       },3000) 
+        // Устанавливаем сообщение и показываем Alert
+        setAlertMessage("Новый пароль был выслан на ваш адрес электронной почты.");
+        setAlertVariant('positive'); // Установите нужный вариант
+        setShowAlert(true);
+        setTimeout(() => {
+            setShowAlert(false)
+        }, 3000)
 
     }
 
     const handleCloseAlert = () => {
-        setAlertVisible(false);
+        setShowAlert(false);
     };
     return (
         <>
@@ -51,32 +67,38 @@ const Password = () => {
                 <p className="text-base ml-1">Забыли пароль?</p>
             </div>
 
-            <Alert 
-                isOpen={alertVisible} 
-                onClose={handleCloseAlert} 
+            <Alert
+                isOpen={isShowAlert}
+                onClose={handleCloseAlert}
                 variant={alertVariant}
             >
                 {alertMessage}
             </Alert>
 
             <form
-                onSubmit={handleSubmit}
+                onSubmit={handleFormSubmit}
+                // onSubmit={handleSubmit}
                 method="dialog"
             >
                 <div className="inline-flex flex-col mt-6">
-                    <label htmlFor="">Email</label>
-                    <input
+
+                    <Input
+                        className={errors.email ? "border-red-500" : ""}
+                        label="Email"
                         type="email"
                         name="email"
-                        value={email} 
+                        value={formData.email}
                         onChange={handleChange}
-                        className="text-center outline-none bg-zinc-300  rounded-lg "
+                        error={errors.email}
                     />
                 </div>
                 <div className="mt-6">
-                    <Link href="/privetofficepage"  className="bg-zinc-300 px-4 py-2 rounded-lg mr-3">назад</Link>
-                    
-                    <button type="submit" className="bg-zinc-300 px-4 py-2 rounded-lg m-3">продолжить</button>
+                    <Link href="/privetofficepage" className="bg-zinc-300 px-4 py-2 rounded-lg mr-3">назад</Link>
+
+                    {/* <button type="submit" className="bg-zinc-300 px-4 py-2 rounded-lg m-3">продолжить</button> */}
+                    <Button type="submit" variant="secondary">
+                        Отправить
+                    </Button>
                 </div>
 
             </form>
